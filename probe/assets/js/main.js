85520d4599dd4aaec3f96fcc27eb821278fba2a0
unknown <hamreni@gmail.com>
2018-03-09 15:22:58 +0100

    add member pictures and underline links

--- probe/assets/js/main.js
+++ probe/assets/js/main.js
@@ -67,7 +67,7 @@
 					bottom:		$header.outerHeight(),
 					terminate:	function() { $header.removeClass('alt'); },
 					enter:		function() { $header.addClass('alt'); },
-					leave:		function() { $header.removeClass('alt'); $header.addClass('reveal'); }
+					leave:		function() { $header.removeClass('alt'); $header.addClass('reveal'); $('.logo img').css({height: 50}) }
 				});
 
 			}
